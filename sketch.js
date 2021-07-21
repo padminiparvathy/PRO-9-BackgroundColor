7f1a389acaaa89c9833ee7a112215a46b1819cd0
padminiparvathy <75385047+padminiparvathy@users.noreply.github.com>
2021-07-21 13:24:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400); 
+
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_gb);
+
+  btn_green = createButton("GREEN");
+  btn_green.position(200,50);
+  btn_green.mousePressed(green_gb);
+}
+
+function draw() {
+  background(r,g,b);
+}
+
+function red_gb()
+{
+  r=255;
+  g=0;
+  b=0;
+}
+
+function green_gb()
+{
+  r=0;
+  g=255;
+  b=0;
+}
